baab8791f76b83cf9cd5e7aa1b07c4984600bbbf
Ethan Smith <ethansmith.dev@gmail.com>
2023-08-29 22:29:22 -0400

    Refined default formatting macro.
    I split the macro up into three macros for ease of use and modularity.
    There is now a with-background macro which sets the background of any
    given output. When used in conjunction with a cell (and a placeholder
    rectangle due to a bug in mcclim) the entire cell background can recieve
    a background color.
    
    I also added some documentation to the functions/macros I have written
    so far.

--- modeline/clim-mode-line/list-formatters.lisp
+++ modeline/clim-mode-line/list-formatters.lisp
@@ -17,33 +17,46 @@
 (defgeneric format-item-display (item frame pane))
 
 (defun cell-height (pane)
+  "returns the height of the application frame, ie the height of the modeline."
   (slot-value (pane-frame pane) 'clim-internals::geometry-height))
 
 (defmacro singleton-table ((stream &rest cell-args) &body body)
+  "creates a table composed of a single cell.
+This macro can be used to set the alignment of content within another
+containter. `cell-args' are passed directly to the formatting-cell."
   `(formatting-table (,stream)
      (formatting-row (,stream)
        (formatting-cell (,stream ,@cell-args)
          ,@body))))
 
-;; TODO implement this macro, so that formatting items can have a default and
-;; configurable style.
-(defmacro with-default-style ((pane &rest args &key (bg +grey+)) &body body)
-  `(let ((output-record
-           (with-output-to-output-record (,pane)
-             (surrounding-output-with-border (,pane)
-                 (formatting-cell (,pane :align-x :left :align-y :top
-                                         :min-height (cell-height ,pane))
-                   (singleton-table (,pane :align-y :center)
-                     ,@body)))))
-         (height (cell-height ,pane)))
+(defmacro with-background ((&optional (stream t) &key (bg +gray+)) &body body)
+  "Draws a rectangle with color `bg' behind `body'.
+`body' is drawn on top of a colored rectangle to apply a background color to it.
+The rectangle will be no larger than the bounding-rectangle of `body'. The
+background and `body' are written to the stream specified by `stream'"
+  `(let* ((output-record (with-output-to-output-record (,stream)
+                           ,@body))
+          (min-point (rectangle-min-point output-record))
+          (max-point (rectangle-max-point output-record)))
 
-     ;; (stream-add-output-record ,pane output-record)
-     (formatting-cell (,pane :align-x :left :align-y :top
-                             :min-height height)
+     (draw-rectangle ,stream min-point max-point :ink ,bg)
+     (stream-add-output-record ,stream output-record)))
 
-       (draw-rectangle* ,pane 0 0
-                        (bounding-rectangle-width output-record) height :ink ,bg)
-       (singleton-table (,pane :align-y :center :min-height height)
+;; TODO add more styling options to this macro to make element shapes more
+;; visually interesting.
+(defmacro with-default-style ((stream &key (bg +grey+)) &body body)
+  "`body' is formatted and drawn to `stream'.
+Every element of the modeline is stored in a cell in a table. This macro creates
+a cell which is placed in that table. `body' is centered vertically and
+horizontally in the cell. A background color specified by `bg' is applied to the
+entire cell."
+  `(formatting-cell (,stream :align-x :left :align-y :top)
+     (with-background (,stream :bg ,bg)
+       ;; NOTE rectangle is a workaround due to a bug with :min-height in mcclim
+       (draw-rectangle* ,stream 0 0 0 (cell-height ,stream)
+                        :ink +transparent-ink+)
+       (singleton-table (,stream :align-x :center :align-y :center
+                                 :min-height (cell-height ,stream))
          ,@body))))
 
 (defstruct spacer
@@ -52,46 +65,44 @@
 (ql:quickload "local-time")
 (defstruct date-time)
 (defmethod format-item-display ((item date-time) frame pane)
-  (formatting-cell (pane :align-y :center)
+  "Displays the local time and date on the modeline."
+  (with-default-style (pane :bg +light-blue+)
+    (local-time::format-timestring
+     t (local-time:now) :format '(" " (:hour 2) ":" (:min 2) " ")))
+  (with-default-style (pane :bg +light-coral+)
     (local-time::format-timestring
      t (local-time:now)
-     :format '((:hour 2) ":" (:min 2) " | " :day " " :short-month ", " :year))))
+     :format '(" " :day " " :short-month ", " :year " "))))
 
-;; BUG Some windows don't show up until they become the active window.
+;; FIXME Some windows don't show up until they become the active window.
 (defstruct windows)
 (defmethod format-item-display ((item windows) frame pane)
+  "displays all windows in the current group.
+
+the selected window has a cyan background color, while marked windows have
+yellow background colors."
   (dolist (win (sort (stumpwm::group-windows (stumpwm::current-group))
                      (lambda (&rest wins)
                        (apply #'< (mapcar #'stumpwm::window-number wins)))))
 
-    (formatting-cell (pane :align-x :center :align-y :top
-                           :min-height (cell-height pane))
-
-      (let* ((name (string-upcase (format nil " ~D: ~A "
-                                           (stumpwm::window-number win)
-                                           (stumpwm::window-class win))))
-             (width (stream-string-width pane name))
-             (height (cell-height pane)))
-
-        (draw-rectangle* pane 0 0 width height :ink
+    (with-default-style (pane :bg
                          (cond ((stumpwm::window-marked win)
                                 +yellow+)
                                ((eq (stumpwm::current-window) win)
                                 +cyan+)
                                (t
                                 +gray80+)))
-
-        (formatting-table (pane)
-          (formatting-row (pane)
-            (formatting-cell (pane :align-y :center :min-height (cell-height pane))
-              (format pane "~A" name))))))))
+      (format pane " ~D: ~A "
+              (stumpwm::window-class win)
+              (stumpwm::window-number win)))))
 
 (defstruct test-item
   (size))
 (defmethod format-item-display ((item test-item) frame pane)
-  (loop for x upto (test-item-size item) do
+  "dummy items to take up space in the modeline"
+  (loop for x below (test-item-size item) do
     (with-default-style (pane)
-      (format t "[TEST ~D]" x))))
+      (format t " [TEST ~D] " x))))
 
 ;; TODO create for matter functions for these items
 (defstruct groups)
@@ -102,6 +113,7 @@
 (defstruct battery)
 
 (defun set-default-modeline ()
+  "sets the default format of the modeline."
   (set-mode-line-format (list (make-windows)
                               (make-test-item :size 4)
                               (make-date-time))))
